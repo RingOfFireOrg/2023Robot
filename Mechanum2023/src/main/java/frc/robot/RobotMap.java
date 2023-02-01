99bef5817e064f817439d5a4b82a8b660ee2b785
Jeremy Whipker <jawtoa@gmail.com>
2023-02-01 12:39:55 -0500

    changed formatting

--- Mechanum2023/src/main/java/frc/robot/RobotMap.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-public class RobotMap {
-    	//Gamepad Axes
-	public static final int MANIPULATOR_RIGHT_TRIGGER = 3;
-	public static final int MANIPULATOR_LEFT_TRIGGER = 2;
-	public static final int MANIPULATOR_LEFT_JOYSTICK_Y = 1;
-	public static final int MANIPULATOR_RIGHT_JOYSTICK_Y = 5;
-}
