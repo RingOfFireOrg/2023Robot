e9686638ab9d14bb3c383e67b94f239d3ff5f2c3
Anoop Kodadhala <73144205+AnoopKodadhala@users.noreply.github.com>
2023-02-14 09:35:13 -0500

    CMD based branch

---
+++ 2023MecanumCMD/src/main/java/frc/robot/Main.java
@@ -0,0 +1,23 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.RobotBase;
+public final class Main {
+  private Main() {}
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hi 
---
+++ 2023MecanumCMD/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.net.PasswordAuthentication;
+
+import com.revrobotics.CANSparkMax;
+
+public class DriveTrain extends SubsystemBase {
+  private final CANSparkMax frontLeft;
+  private final CANSparkMax frontRight;
+  private final CANSparkMax backLeft;
+  private final CANSparkMax backRight;
+
+
+  public DriveTrain() {
+    double rf,rb,lf,lb;
+ 
+  }
+
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+
+
+  }
+}
