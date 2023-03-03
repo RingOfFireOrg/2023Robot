79c9d2af9c3e2bfd2e38eeb708b981c367908df5
Abhi10121 <uabhi2005@gmail.com>
2023-03-02 21:55:02 -0500

    extender and intake testing

--- src/main/java/frc/robot/Extender.java
+++ src/main/java/frc/robot/Extender.java
@@ -6,6 +6,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 
 
@@ -17,28 +18,54 @@ public class Extender extends TeleopModule{
     public double extender1Position;
     public double extender2Position;
     public DutyCycleEncoder encoder;
+    public MotorControllerGroup extender;
     public double encoderPosition;
               
     public Extender () {
         extenderMotor1 = new CANSparkMax(14, MotorType.kBrushless);
         extenderMotor2 = new CANSparkMax(15, MotorType.kBrushless);
+        extender = new MotorControllerGroup(extenderMotor1, extenderMotor2);
         encoder = new DutyCycleEncoder(0);
         encoder.setDistancePerRotation(4.0);
+        extenderMotor1.setInverted(true);
+        extenderMotor2.setInverted(true);
     }
 
     public void robotInit() {
+        encoder.reset();
     }
 
     public void teleopInit() {
+
     }
 
     public void teleopControl() {
-        extenderMotor2.set(ControlSystems.getInstance().gamepadLeftY());
-        extenderMotor1.set(ControlSystems.getInstance().gamepadRightY());
-        encoderPosition = encoder.getDistance();
+        if(ControlSystems.getInstance().gamepadRightY() < -0.1 || ControlSystems.getInstance().gamepadRightY() > 0.1) {
+            extender.set(ControlSystems.getInstance().gamepadRightY());
+        }
+        else if(encoderPosition > 44 && ControlSystems.getInstance().gamepadA()) {
+            extender.set(-.2);
+        }
+        else if(encoderPosition < 42 && ControlSystems.getInstance().gamepadA()) {
+            extender.set(.8);
+        }
+        else if(encoderPosition < 4 && ControlSystems.getInstance().gamepadB()) {
+            extender.set(.3);
+        }
+        else if(encoderPosition > 6 && ControlSystems.getInstance().gamepadB()) {
+            extender.set(-0.4);
+        }
+        else {
+            extender.set(0);
+        } 
 
+        //if(encoderPosition < )
+
+        encoderPosition = encoder.getDistance();
         SmartDashboard.putNumber("Extender Position: ", encoderPosition);
     }
+
+
     public void periodic() {
     }
 }
--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,81 +1,79 @@
-// package frc.robot;
+package frc.robot;
 
-// import edu.wpi.first.wpilibj.DoubleSolenoid;
-// import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-// import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
-// import edu.wpi.first.wpilibj.Compressor;
-// import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-// import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// public class Intake extends TeleopModule{
-//     CANSparkMax intakeActuator;
-//     RelativeEncoder intakeEncoder;
+public class Intake extends TeleopModule{
+    CANSparkMax intakeActuator;
+    RelativeEncoder intakeEncoder;
 
-//     double intakePosition;
+    double intakePosition;
 
-//     DoubleSolenoid intake;
-//     DoubleSolenoid piston2;
+    DoubleSolenoid intake;
+    DoubleSolenoid piston2;
     
-//     public Intake() {
-//         intakeActuator = new CANSparkMax(RobotMap.INTAKE_ACTUATOR, MotorType.kBrushless);
-//         intakeEncoder = intakeActuator.getEncoder();
-//         intakeActuator.setInverted(true);
-//         intake = new DoubleSolenoid (PneumaticsModuleType.CTREPCM, 1, 0);
-//         //piston1 = new DoubleSolenoid (3, null, 2, 4);
-//     }
+    public Intake() {
+        intakeActuator = new CANSparkMax(RobotMap.INTAKE_ACTUATOR, MotorType.kBrushless);
+        intakeEncoder = intakeActuator.getEncoder();
+        intakeActuator.setInverted(true);
+        intake = new DoubleSolenoid (PneumaticsModuleType.CTREPCM, 1, 0);
+        //piston1 = new DoubleSolenoid (3, null, 2, 4);
+    }
 
-//     @Override
-//     public void teleopControl() {
-//         if(ControlSystems.getInstance().gamepadB()) {
-//             intake.set(Value.kForward);
-//         } else if (ControlSystems.getInstance().gamepadA()) {
-//             intake.set(Value.kReverse);
-//         } else {
-//             intake.set(Value.kOff);
-//         }
+    @Override
+    public void teleopControl() {
+        if(ControlSystems.getInstance().gamepadB()) {
+            intake.set(Value.kForward);
+        } else if (ControlSystems.getInstance().gamepadA()) {
+            intake.set(Value.kReverse);
+        } else {
+            intake.set(Value.kOff);
+        }
         
-//         if(ControlSystems.getInstance().gamepadRightY() < -0.1 || ControlSystems.getInstance().gamepadRightY() > 0.1) {
-//             intakeActuator.set(ControlSystems.getInstance().gamepadRightY()*0.4);
-//         }
-//         else if(intakePosition < -7 && ControlSystems.getInstance().gamepadX()) {
-//             intakeActuator.set(0.4);
-//         } else if (intakePosition > -5 && ControlSystems.getInstance().gamepadX()) {
-//             intakeActuator.set(-0.4);
-//         }
-//         else if(intakePosition < -60 && ControlSystems.getInstance().gamepadY()) {
-//             intakeActuator.set(0.4);
-//         } else if (intakePosition > -58 && ControlSystems.getInstance().gamepadY()) {
-//             intakeActuator.set(-0.6);
-//         }
-        
-        
-//         else {
-//             intakeActuator.set(0);
-//         } 
+        if(ControlSystems.getInstance().gamepadRightY() < -0.1 || ControlSystems.getInstance().gamepadRightY() > 0.1) {
+            intakeActuator.set(ControlSystems.getInstance().gamepadRightY()*0.4);
+        }
+        else if(intakePosition < -7 && ControlSystems.getInstance().gamepadX()) {
+            intakeActuator.set(0.4);
+        } else if (intakePosition > -5 && ControlSystems.getInstance().gamepadX()) {
+            intakeActuator.set(-0.4);
+        }
+        else if(intakePosition < -60 && ControlSystems.getInstance().gamepadY()) {
+            intakeActuator.set(0.4);
+        } else if (intakePosition > -58 && ControlSystems.getInstance().gamepadY()) {
+            intakeActuator.set(-0.6);
+        }
+        else {
+            intakeActuator.set(0);
+        } 
         
         
-//         intakePosition = intakeEncoder.getPosition();
-//         SmartDashboard.putNumber("Intake Position", intakePosition);
-//     }
+        intakePosition = intakeEncoder.getPosition();
+        SmartDashboard.putNumber("Intake Position", intakePosition);
+    }
 
-//     @Override
-//     public void teleopInit() {
-//         // TODO Auto-generated method stub
+    @Override
+    public void teleopInit() {
+        // TODO Auto-generated method stub
         
-//     }
-//     @Override
-//     public void periodic() {
-//         // TODO Auto-generated method stub
+    }
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
         
-//     }
-//     @Override
-//     public void robotInit() {
+    }
+    @Override
+    public void robotInit() {
         
-//     }
-// }
+    }
+}
